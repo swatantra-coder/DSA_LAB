f7ff9d5f2b5083947364448fbc4d577f1e0f4ab0
Swatantra Singh <68724074+swatantra-coder@users.noreply.github.com>
2022-02-01 10:04:22 +0530

    Update Searching.c

--- LAB1/Searching.c
+++ LAB1/Searching.c
@@ -36,3 +36,50 @@ int main()
 
     return 0;
 }
+
+#include<iostream>
+using namespace std;
+void bubbleSort(int* a,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[i] > a[j])
+			{
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int n;
+	printf("Enter the length of array:");
+	scanf("%d",&n);
+	int a[1000];
+	for (int i = 0; i < n; i++)
+	{   printf("Enter the number:");
+		scanf("%d",&a[i]);
+	}
+	bubbleSort(a, n);
+	
+	if (n >= 2)
+	{
+		printf("second minimum: %d\n",a[1]);
+		printf("second maximum: %d",a[n - 2]);
+		return 0;
+	}
+	else
+	{
+		printf("second minimum: %d",a[0]);
+		printf("second maximum: %d",a[0]);
+		return 0;
+	}
+	return 0;
+		
+
+	
+}
